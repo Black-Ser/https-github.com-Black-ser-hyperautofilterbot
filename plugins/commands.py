560490dc717ed6c446288756f9ac0de44ed21910
hyperser <110296643+hyperser@users.noreply.github.com>
2023-01-01 10:58:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -244,6 +244,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('Test Button', url='https://t.me/moviemania_tg') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
